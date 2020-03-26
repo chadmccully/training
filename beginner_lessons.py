8ae559f9c0ba71fbda5d5bea03a6630d7c134f6e
chadmccully <chad.mccully@hotmail.com>
2020-03-26 09:26:49 -0500

    Initial commit

---
+++ beginner_lessons.py
@@ -0,0 +1,30 @@
+#tuples and indexses
+# numbers = [1,4,6,3,4]
+# # for index, number in enumerate(numbers):
+# #     print(f'{index} - {number}')
+# #
+# # values = list('aeiou')
+# #
+# # ['a', 'e', 'i', 'o', 'u']
+# # for index, vowel in enumerate(values):
+# # ...     print(f'{index} - {vowel}')
+
+
+# number = 5
+# if(number%2==0):
+#     isEven = True
+# else:
+#     isEven = False
+# #or
+# isEven = True if number%2==0 else False
+# #or
+# isEven = number%2==0
+
+
+# sum([12,34,56])
+#
+# number1 = 10
+# number2 = 20
+# sum = number1 + number2 <== this creates shadowing of the function sum().  You can do this better by including an underscore (sum_)
+
+
